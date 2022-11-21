f6a3bcdbf76b529d2a2e5411e1af9d0be490e21e
antcodernez <marshday5@gmail.com>
2022-11-20 22:52:36 -0600

    I learned about strings and array methods and about math object

---
+++ cap4/code.js
@@ -0,0 +1,120 @@
+// Métodos de cadenas
+
+// .conCat()
+
+// let cadena = "cadena de prueba ";
+// let cadena2 = "cadena 2"
+// let result = cadena.concat(cadena2);
+
+// document.write(result);
+
+// .startsWith() endsWith() si una cadena empieza con los caracteres de otra cadena
+
+// let cadena = "cadena de prueba";
+// let cadena2 = "cadena"
+// let result = cadena.startsWith(cadena2);
+// let result2 = cadena.endsWith(cadena2);
+
+// document.write(result + "<br>");
+
+// document.write(result2);
+
+// .includes() busca cadenas
+
+// let cadena = "cadena de prueba algo";
+// let cadena2 = "cadena";
+
+// let result = cadena.includes(cadena2);
+
+// document.write(result);
+
+// .indexOf() regresa la posición de mi caracter
+
+// let cadena = "cadena de prueba algo";
+// let cadena2 = "cadena";
+
+// let result = cadena.indexOf("d");
+
+// document.write(result);
+
+// lastIndexOf()
+
+// let cadena = "Pedro es un tarado y huele a popo, popo, popo";
+// let cadena2 = "";
+
+// let resultado = cadena.lastIndexOf("popo");
+
+// document.write(resultado);
+
+// padStart()
+
+// let cadena = "abc";
+
+// let resultado = cadena.padStart(9, "xd ");
+
+// document.write(resultado);
+
+// padEnd(CantidadDeseadaRellenar, "String");
+
+// let cadena = "abc";
+
+// let resultado = cadena.padStart(9, "xd ");
+
+// document.write(resultado);
+
+// .repeat()
+
+// let cadena = "Algo xd ";
+
+// resultado = cadena.repeat(2);
+
+// document.write(resultado);
+
+// .split()
+
+// let cadena = "hola, ¿como ,estas";
+
+// resultado = cadena.split(",");
+
+// document.write(resultado[0]);
+
+// .subString()
+
+// let cadena = "ABCDEFG";
+
+// resultado = cadena.substring(0,3);
+
+// document.write(resultado);
+
+// .toLowerCase()
+
+// let cadena = "ABCDEFG";
+// resultado = cadena.toLowerCase();
+// document.write(resultado);
+
+// .toUpperCase()
+
+// let cadena2 = "ABCDEFG";
+// resultado = cadena.toUpperCase();
+// document.write(resultado);
+
+// .trim()
+
+// let cadena2 = "  ABCDEFG  ";
+// resultado = cadena.trim();
+// document.write(resultado);
+
+// .trimEnd()
+// let cadena2 = "  ABCDEFG  ";
+// resultado = cadena.trimEnd();
+// document.write(resultado);
+
+// .trimStart()
+
+// let cadena = "  ABCDEFG  ";
+// resultado = cadena.trim();
+// document.write(resultado);
+
+
+// METODOS DE ARRAYS
+

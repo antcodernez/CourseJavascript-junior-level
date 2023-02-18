7750090f427ee5d49ebd1257e11570e29ed5e765
Antonio Jiménez <marshday5@gmail.com>
2023-02-18 14:13:57 -0600

    Notes about methods about window just like alert(), print(), confirm(), etc

---
+++ cap7/cap7.js
@@ -0,0 +1,36 @@
+// Window
+//.open() carga un recurso en el contexto de un nuevo navegador o uno que ya existe
+
+let urlProyect = "https://antcodernez.github.io/responsiveDesingProyect/";
+// let ventana = window.open(urlProyect);
+
+// .close() Cierra ventanas especificas o actuales
+// ventana.close();
+
+// closed verifica si la ventana referenciada esta cerrada o no
+// let openOrClosed = ventana.closed;
+// console.log(openOrClosed); // retorna un true porque ventana esta cerrada
+
+// .stop() detiene la carga de una pagina
+// let urlPlatzi = "https://platzi.com/";
+// window.open(urlPlatzi).stop();
+
+// alert()
+// alert("Ya sabes, seas mamon we");
+
+// .print()
+// print() // me deja imprimir con js
+
+// prompt
+// prompt("Tu nombre: ");
+
+// confirm()
+// let xd = confirm("Seguro que quieres salir del sitio web?");
+// console.log(xd); retorna boleanos
+
+// scren
+// const screen = window.screen;
+// console.log(screen);
+// document.write(screen);
+
+//
